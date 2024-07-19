2fd669011983a5a1c1d8ce361f5bdc5707eba39b
Noziphezinhle Nzimande <125486198+Nozzie-Dev@users.noreply.github.com>
2024-07-19 15:43:06 +0200

    create objects

--- javascript.js
+++ javascript.js
@@ -67,9 +67,63 @@ document.getElementById('paymentForm').addEventListener('submit', function(event
     }
 
     if (isValid) {
-        // Submit the form or do whatever is needed
-        alert('Form is valid');
+        // Add code to proceed to buy tickets
+        // pop-up to get quantity, if 1> get atendee names
+        // alert tickets have been emailed to user email
+        
     } else {
         alert('Please fill out all fields correctly.');
     }
 });
+
+
+//Objects
+class Event {
+    constructor(eventId, title, description, startDate, endDate, location, price, availableTickets) {
+        this.eventId = eventId;
+        this.title = title;
+        this.description = description;
+        this.startDate = new Date(startDate);
+        this.endDate = new Date(endDate);
+        this.location = location;
+        this.price = price;
+        this.availableTickets = availableTickets;
+    }
+
+    isSoldOut() {
+        return this.availableTickets <= 0;
+    }
+
+    updateTicketAvailability(ticketsSold) {
+        if (ticketsSold > this.availableTickets) {
+            throw new Error('Not enough tickets available');
+        }
+        this.availableTickets -= ticketsSold;
+    }
+}
+
+class Ticket {
+    constructor(ticketId, eventId, userId, purchaseDate, price) {
+        this.ticketId = ticketId;
+        this.eventId = eventId;
+        this.userId = userId;
+        this.purchaseDate = new Date(purchaseDate);
+        this.price = price;
+    }
+}
+
+class User {
+    constructor(userId, username, password, email, fullName) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.fullName = fullName;
+    }
+
+    validatePassword(passwordToCheck) {
+        return this.password === passwordToCheck;
+    }
+}
+
+//Event Schedule
