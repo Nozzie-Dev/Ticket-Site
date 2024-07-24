9581c6270e2a47bfcf766bbbb785c6aeb388ff2c
Noziphezinhle Nzimande <125486198+Nozzie-Dev@users.noreply.github.com>
2024-07-24 13:51:15 +0200

    resolve logic errors

--- javascript.js
+++ javascript.js
@@ -1,33 +1,3 @@
-//Scripts for register
-function validateForm(event) {
-    event.preventDefault();
-    const fields = ['name', 'email', 'address', 'city', 'province', 'postalCode'];
-    let valid = true;
-
-    fields.forEach(function(field) {
-        const input = document.getElementById(field);
-        if (!input.value) {
-            input.classList.add('error');
-            valid = false;
-        } else {
-            input.classList.remove('error');
-        }
-    });
-
-    if (valid) {
-        document.getElementById('registerForm').submit();
-    } else {
-        alert("All fields must be filled out");
-    }
-}
-
-document.getElementById('registerForm').addEventListener('submit', validateForm);
-
-function onSignIn(response) {
-    console.log(response); // Handle the response from Google Sign-In
-}
-
-
 
 //Objects
 class Event {
@@ -64,6 +34,7 @@ class Ticket {
     }
 }
 
+
 // Define the User class
 class User {
     constructor(userId, username, password, email, fullName) {
@@ -96,6 +67,32 @@ function authenticateUser(username, password) {
     return users.find(user => user.getUsername() === username && user.validatePassword(password));
 }
 
+//Scripts for register
+function validateForm(event) {
+    event.preventDefault();
+    const fields = ['name', 'email', 'address', 'city', 'province', 'postalCode'];
+    let valid = true;
 
+    fields.forEach(function(field) {
+        const input = document.getElementById(field);
+        if (!input.value) {
+            input.classList.add('error');
+            valid = false;
+        } else {
+            input.classList.remove('error');
+        }
+    });
 
+    if (valid) {
+        document.getElementById('registerForm').submit();
+    } else {
+        alert("All fields must be filled out");
+    }
+}
+
+document.getElementById('registerForm').addEventListener('submit', validateForm);
+
+function onSignIn(response) {
+    console.log(response); // Handle the response from Google Sign-In
+}
 
